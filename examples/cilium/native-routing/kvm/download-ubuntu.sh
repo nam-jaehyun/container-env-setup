de70a909f0799f06ebb9b3279c9d6fe0cea8078b
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-04-23 06:29:55 +0000

    update script

---
+++ examples/cilium/native-routing/kvm/download-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir ~/images; cd ~/images
+
+#wget http://sdxdata.com/downloads/ubuntu-16.04.2-server-amd64.iso
+wget http://sdxdata.com/downloads/ubuntu-18.04.5-live-server-amd64.iso
